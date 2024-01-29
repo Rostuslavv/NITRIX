095b4cdabdbae35dec23adf167a0e65b4527521f
Rostyslav Dydiak <rostuslavdz@gmail.com>
2024-01-29 20:58:04 +0200

    git init

---
+++ NITRIX/NITRIX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NITRIX
+//
+//  Created by Rostyslav on 29.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
